312c4ec4faa0fbc15f07b5077daecbbaa946eb17
Jaanika-L <jaanika.lyys@gmail.com>
2023-03-06 20:36:09 +0200

    Reacti algus

---
+++ veebipood/src/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img className="pilt" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Ours_brun_parcanimalierpyrenees_1.jpg/220px-Ours_brun_parcanimalierpyrenees_1.jpg" alt="Karu" />
+    <button className="nupp">Nupp</button>
+    {/* <p></p>
+    <div>Tekst</div> */}
+    {/* <span></span> */}
+    </div>
+  );
+}
+
+export default App;
