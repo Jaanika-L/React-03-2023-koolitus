9a180d6d36c5c0615fdb2939adbbacfbf6679097
Jaanika-L <jaanika.lyys@gmail.com>
2023-03-07 14:18:53 +0200

    kodutoo1

--- kodutoo1/src/App.js
+++ kodutoo1/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <button className="nupp">Press</button> 
     <p className="tekst1">Hello World!</p>     
     <p className="tekst2">Tere Maailm! </p>
-    <img className="pilt" src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="Javascripti logo"></img>
+    <img className="pilt" src="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" alt="Javascripti logo"/>
     </div>
   );
 }
